029d2c1b3e63f345967eff6f7f860845b8d11eb4
Alan Felix <alangiovani123@gmail.com>
2024-02-08 18:14:53 -0600

    Add: Create the DTO's for the correct manipulation of the data

--- src/App/main.ts
+++ src/App/main.ts
@@ -1,26 +1,24 @@
 import {faker} from '@faker-js/faker';
-import {AddProduct, GetProducts} from './Products/poducts.service';
+import {AddProduct, CreateProduct, GetProducts, UpdateProduct, SearchProduct} from './Products/products.service';
 
 for(let i= 0; i<10; i++){
-  AddProduct({
-    id: faker.string.uuid(),
+  CreateProduct({
     title: faker.commerce.productName(),
-    createdAt: faker.date.recent(),
-    updatedAt: faker.date.recent(),
     image: faker.image.url(),
     stock: faker.number.float({min:10,max:100}),
     color: faker.color.human(),
     isNew: faker.datatype.boolean(),
     price: faker.commerce.price(),
     tags:faker.helpers.arrayElements(['technologie','fashion','shorts','shirts','hats', 'jackets']),
-    categorie : {
-      id: faker.string.uuid(),
-      createdAt:faker.date.recent(),
-      updatedAt:faker.date.recent(),
-      name: faker.commerce.department()
-    },
-    size:faker.helpers.arrayElement(['S','L','M','L','XL'])
+    size:faker.helpers.arrayElement(['S','L','M','L','XL']),
+    categoryId: faker.string.uuid()
   });
 }
+
+UpdateProduct('12',{});
+
+SearchProduct({
+  
+});
 
 console.log(GetProducts());
